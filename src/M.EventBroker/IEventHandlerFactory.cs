d5e7087134e1357bc316bc9ffa8c9764f6ba6330
petar <petar_m@mail.bg>
2018-05-24 15:21:42 +0300

    Comments

--- src/M.EventBroker/IEventHandlerFactory.cs
+++ src/M.EventBroker/IEventHandlerFactory.cs
@@ -2,8 +2,16 @@
 
 namespace M.EventBroker
 {
+    /// <summary>
+    /// Represents a provider for event handling instances.
+    /// </summary>
     public interface IEventHandlerFactory
     {
+        /// <summary>
+        /// Returns an event handling instances.
+        /// </summary>
+        /// <typeparam name="TEvent">The type of the event.</typeparam>
+        /// <returns>An IEnumerable of event handlers. Null is valid return value.</returns>
         IEnumerable<IEventHandler<TEvent>> HandlersFor<TEvent>();
     }
 }
