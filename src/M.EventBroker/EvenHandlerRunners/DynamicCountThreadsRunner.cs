1e22a41dcf3b87aaa385b7312bcff54b394e4d58
petar <petar_m@mail.bg>
2018-06-17 21:18:16 +0300

    Added DynamicCountThreadsRunner class.

---
+++ src/M.EventBroker/EvenHandlerRunners/DynamicCountThreadsRunner.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace M.EventBroker.EvenHandlerRunners
+{
+    /// <summary>
+    /// Runs event handlers on background threads, creating and releasing threads depending on the load.
+    /// </summary>
+    public class DynamicCountThreadsRunner : IEventHandlerRunner
+    {
+        private readonly TimeSpan _timeout = TimeSpan.FromMilliseconds(100);
+        private readonly BlockingCollection<Action> _handlerActions = new BlockingCollection<Action>();
+        private readonly Stack<ThreadInfo> _runningThreads = new Stack<ThreadInfo>();
+        private bool _isRunning;
+        private readonly DynamicCountThreadsRunnerConfig _config;
+
+        /// <summary>
+        /// Creates a new instance of the DynamicCountThreadsRunner class.
+        /// </summary>
+        /// <param name="config">Specifies configuration for handler threads.</param>
+        public DynamicCountThreadsRunner(DynamicCountThreadsRunnerConfig config)
+        {
+            _config = config;
+            _isRunning = true;
+
+            Thread tracker = new Thread(Tracker);
+            tracker.Start();
+
+            StartWorkerThread();
+        }
+
+        /// <summary>
+        /// Runs events handlers on available backgrround thread.
+        /// </summary>
+        /// <param name="handlers">The event handlers to run.</param>
+        public void Run(params Action[] handlers)
+        {
+            foreach (var handler in handlers)
+            {
+                _handlerActions.Add(handler);
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (_isRunning)
+            {
+                _isRunning = false;
+                while(_runningThreads.Count > 0)
+                {
+                    var threadInfo = _runningThreads.Pop();
+                    threadInfo.IsRunning = false;
+                }
+
+                Thread.Sleep(_timeout > _config.CheckInterval ? _timeout : _config.CheckInterval);
+            }
+
+            _handlerActions.Dispose();
+        }
+
+        private void Tracker()
+        {
+            int cyclesUnderTreshold = 0, cyclesOverTreshold = 0;
+            while (_isRunning)
+            {
+                int waitingHandlersCount = _handlerActions.Count;
+                bool isOverTreshold = waitingHandlersCount > _config.WaitingHandlersCountTreshold;
+                if (isOverTreshold)
+                {
+                    cyclesOverTreshold++;
+                    cyclesUnderTreshold = 0;
+                    if (cyclesOverTreshold > _config.CyclesBeforeAddingThread)
+                    {
+                        cyclesOverTreshold = 0;
+                        StartWorkerThread();
+                    }
+                }
+                else
+                {
+                    cyclesUnderTreshold++;
+                    cyclesOverTreshold = 0;
+                    if (cyclesUnderTreshold > _config.CyclesBeforeReleasingThread)
+                    {
+                        cyclesUnderTreshold = 0;
+                        ReleaseWorkerThread();
+                    }
+                }
+
+                Thread.Sleep(_config.CheckInterval);
+            }
+        }
+
+        private void StartWorkerThread()
+        {
+            if(_runningThreads.Count > _config.MaxThreadsCount)
+            {
+                return;
+            }
+
+            var workerThread = new Thread(Worker);
+            var threadInfo = new ThreadInfo { IsRunning = true };
+            _runningThreads.Push(threadInfo);
+            workerThread.Start(threadInfo);
+        }
+
+        private void ReleaseWorkerThread()
+        {
+            if(_runningThreads.Count == 1)
+            {
+                return;
+            }
+
+            ThreadInfo threadInfo = _runningThreads.Pop();
+            threadInfo.IsRunning = false;
+        }
+
+        private void Worker(object parameter)
+        {
+            var threadInfo = (ThreadInfo)parameter;
+            while (_isRunning)
+            {
+                if (!threadInfo.IsRunning)
+                {
+                    return;
+                }
+
+                if (!_handlerActions.TryTake(out Action action, _timeout))
+                {
+                    continue;
+                }
+
+                action();
+            }
+        }
+
+        private class ThreadInfo
+        {
+            public bool IsRunning { get; set; }
+        }
+    }
+}
---
+++ src/M.EventBroker/EvenHandlerRunners/DynamicCountThreadsRunnerConfig.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace M.EventBroker.EvenHandlerRunners
+{
+    /// <summary>
+    /// Specifies configuration parameters for DynamicCountThreadsRunner class.
+    /// </summary>
+    public class DynamicCountThreadsRunnerConfig
+    {
+        /// <summary>
+        /// Creates a new instance of the DynamicCountThreadsRunnerConfig class.
+        /// </summary>
+        /// <param name="checkInterval">Specifies how often to check the treshold.</param>
+        /// <param name="waitingHandlersCountTreshold">Specifies how many handlers waiting to be runned should trigger creation of new worker thread.</param>
+        /// <param name="cyclesBeforeAddingThread">Specifies the number of consequent check cycles with exceeded treshold trigger creation of new worker thread.</param>
+        /// <param name="cyclesBeforeReleasingThread">Specifies the number of consequent check cycles under treshold trigger releasing of worker thread.</param>
+        /// <param name="maxThreadsCount">Specifies the maximum number of worker threads.</param>
+        public DynamicCountThreadsRunnerConfig(
+            TimeSpan checkInterval,
+            int waitingHandlersCountTreshold,
+            int cyclesBeforeAddingThread,
+            int cyclesBeforeReleasingThread,
+            int maxThreadsCount)
+        {
+            WaitingHandlersCountTreshold = waitingHandlersCountTreshold > 0 ? waitingHandlersCountTreshold : throw new ArgumentOutOfRangeException($"Parameter {nameof(waitingHandlersCountTreshold)} should be positive integer (value was: {waitingHandlersCountTreshold})");
+            CheckInterval = checkInterval > TimeSpan.Zero ? checkInterval : throw new ArgumentOutOfRangeException($"Parameter {nameof(checkInterval)} should be positive TimeSpan (value was: {checkInterval})");
+            MaxThreadsCount = maxThreadsCount > 0 ? maxThreadsCount : throw new ArgumentOutOfRangeException($"Parameter {nameof(maxThreadsCount)} should be positive integer (value was: {maxThreadsCount})");
+            CyclesBeforeAddingThread = cyclesBeforeAddingThread > 0 ? cyclesBeforeAddingThread : throw new ArgumentOutOfRangeException($"Parameter {nameof(cyclesBeforeAddingThread)} should be positive integer (value was: {cyclesBeforeAddingThread})");
+            CyclesBeforeReleasingThread = cyclesBeforeReleasingThread > 0 ? cyclesBeforeReleasingThread : throw new ArgumentOutOfRangeException($"Parameter {nameof(cyclesBeforeReleasingThread)} should be positive integer (value was: {cyclesBeforeReleasingThread})");
+        }
+
+        /// <summary>
+        /// How often to check the treshold.
+        /// </summary>
+        public TimeSpan CheckInterval { get; }
+
+        /// <summary>
+        /// How many handlers waiting to be runned should trigger creation of new worker thread.
+        /// </summary>
+        public int WaitingHandlersCountTreshold { get; }
+
+        /// <summary>
+        /// Maximum number of worker threads.
+        /// </summary>
+        public int MaxThreadsCount { get; }
+
+        /// <summary>
+        /// Number of consequent check cycles under treshold trigger before releasing of worker thread.
+        /// </summary>
+        public int CyclesBeforeReleasingThread { get; }
+
+        /// <summary>
+        /// Number of consequent check cycles with exceeded treshold trigger creation of new worker thread.
+        /// </summary>
+        public int CyclesBeforeAddingThread { get; }
+
+        /// <summary>
+        /// Creates a new instance of the DynamicCountThreadsRunnerConfig class with default values:
+        /// checkInterval: TimeSpan.FromMilliseconds(100)
+        /// waitingHandlersCountTreshold: 2
+        /// cyclesBeforeAddingThread: 2
+        /// cyclesBeforeReleasingThread: 20
+        /// maxThreadsCount: 5
+        /// </summary>
+        /// <returns>A new instance of the DynamicCountThreadsRunnerConfig class initialized with default values.</returns>
+        public static DynamicCountThreadsRunnerConfig Default()
+        {
+            return new DynamicCountThreadsRunnerConfig(
+                checkInterval: TimeSpan.FromMilliseconds(100),
+                waitingHandlersCountTreshold: 2,
+                cyclesBeforeAddingThread: 2,
+                cyclesBeforeReleasingThread: 20,
+                maxThreadsCount: 5);
+        }
+    }
+}
